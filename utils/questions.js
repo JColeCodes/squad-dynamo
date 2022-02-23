390321593ab860d828c84d7e657327359f7e0995
Jennifer Cole <capauldi@gmail.com>
2022-02-23 04:22:37 -0800

    engineer and intern prompt questions

--- utils/questions.js
+++ utils/questions.js
@@ -1,10 +1,12 @@
 const inquirer = require('inquirer');
 
+// Require classes
 const Employee = require('../lib/Employee');
 const Engineer = require('../lib/Engineer');
 const Intern = require('../lib/Intern');
 const Manager = require('../lib/Manager');
 
+// Profiles stores data
 function Profiles() {
     this.manager = [];
     this.engineers = [];
@@ -12,8 +14,9 @@ function Profiles() {
 }
 
 const profiles = new Profiles;
-const employeeIds = [];
+const employeeIds = []; // Array of input ids so each employee has a unique id
 
+// Prompt for manager
 const promptManager = () => {
     console.log('Welcome to the Team Profile Generator!');
     return inquirer.prompt([
@@ -31,11 +34,11 @@ const promptManager = () => {
             }
         },
         {
-            type: 'number',
+            type: 'input',
             name: 'managerId',
             message: 'Input the Manager\'s employee id:',
             validate: managerIdInput => {
-                if (!employeeIds.includes(managerIdInput)) {
+                if (!isNaN(managerIdInput) && !employeeIds.includes(managerIdInput)) {
                     employeeIds.push(managerIdInput);
                     return true;
                 } else {
@@ -72,6 +75,7 @@ const promptManager = () => {
         }
     ])
     .then(managerData => {
+        // Creates a new Manager
         Profiles.prototype.newManager = function() {
             const manager = new Manager(
                 managerData.managerName, 
@@ -82,8 +86,175 @@ const promptManager = () => {
             this.manager.push(manager);
         };
         profiles.newManager();
-        //console.log(profiles);
-        //console.log(profiles.manager[0].getRole());
+    });
+}
+
+// Prompt for the other profiles - Engineer and Intern
+const promptProfiles = () => {
+    return inquirer.prompt([
+        {
+            type: 'list',
+            name: 'continueChoice',
+            message: 'What would you like to do next?',
+            choices: ['Add an Engineer', 'Add an Intern', 'Finish Building (Generate Page)']
+        },
+    ])
+    .then(choice => {
+        // If adding Engineer
+        if (choice.continueChoice === 'Add an Engineer') {
+            return inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'engineerName',
+                    message: 'Input the Engineer\'s name:',
+                    validate: engineerNameInput => {
+                        if (engineerNameInput) {
+                            return true;
+                        } else {
+                            console.log('\nPlease input the Engineer\'s name:');
+                            return false;
+                        }
+                    }
+                },
+                {
+                    type: 'input',
+                    name: 'engineerId',
+                    message: 'Input the Engineer\'s employee id:',
+                    validate: engineerIdInput => {
+                        if (!isNaN(engineerIdInput) && !employeeIds.includes(engineerIdInput)) {
+                            employeeIds.push(engineerIdInput);
+                            return true;
+                        } else {
+                            console.log('\nPlease input a unique numerical id:');
+                            return false;
+                        }
+                    }
+                },
+                {
+                    type: 'input',
+                    name: 'engineerEmail',
+                    message: 'Input the Engineer\'s email:',
+                    validate: engineerEmailInput => {
+                        if (engineerEmailInput.includes("@") && engineerEmailInput.includes(".")) {
+                            return true; // Makes sure email includes @ and .
+                        } else {
+                            console.log('\nPlease input a valid email:');
+                            return false;
+                        }
+                    }
+                },
+                {
+                    type: 'input',
+                    name: 'engineerGitHub',
+                    message: 'Input the Engineers\'s GitHub username:',
+                    validate: engineerGitHubInput => {
+                        if (engineerGitHubInput) {
+                            return true;
+                        } else {
+                            console.log('\nPlease input a GitHub username:');
+                            return false;
+                        }
+                    }
+                }
+            ])
+        }
+        // If adding an intern
+        else if (choice.continueChoice === 'Add an Intern') {
+            return inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'internName',
+                    message: 'Input the Intern\'s name:',
+                    validate: internNameInput => {
+                        if (internNameInput) {
+                            return true;
+                        } else {
+                            console.log('\nPlease input the Intern\'s name:');
+                            return false;
+                        }
+                    }
+                },
+                {
+                    type: 'input',
+                    name: 'internId',
+                    message: 'Input the Intern\'s employee id:',
+                    validate: internIdInput => {
+                        if (!isNaN(internIdInput) && !employeeIds.includes(internIdInput)) {
+                            employeeIds.push(internIdInput);
+                            return true;
+                        } else {
+                            console.log('\nPlease input a unique numerical id:');
+                            return false;
+                        }
+                    }
+                },
+                {
+                    type: 'input',
+                    name: 'internEmail',
+                    message: 'Input the Intern\'s email:',
+                    validate: internEmailInput => {
+                        if (internEmailInput.includes("@") && internEmailInput.includes(".")) {
+                            return true; // Makes sure email includes @ and .
+                        } else {
+                            console.log('\nPlease input a valid email:');
+                            return false;
+                        }
+                    }
+                },
+                {
+                    type: 'input',
+                    name: 'internSchool',
+                    message: 'Input the Intern\'s school:',
+                    validate: internSchoolInput => {
+                        if (internSchoolInput) {
+                            return true;
+                        } else {
+                            console.log('\nPlease input the name of a school:');
+                            return false;
+                        }
+                    }
+                }
+            ])
+        }
+        // Else (Exit)
+        else {
+            return;
+        }
+    })
+    .then(profileData => {
+        // Create new Engineer
+        if (profileData.engineerName) {
+            Profiles.prototype.newEngineer = function() {
+                const engineer = new Engineer(
+                    profileData.engineerName, 
+                    profileData.engineerId, 
+                    profileData.engineerEmail, 
+                    profileData.engineerGitHub
+                );
+                this.engineers.push(engineer);
+            };
+            profiles.newEngineer();
+            promptProfiles(); // Call function again
+        }
+        // Create new Intern
+        else if (profileData.internName) {
+            Profiles.prototype.newIntern = function() {
+                const intern = new Intern(
+                    profileData.internName, 
+                    profileData.internId, 
+                    profileData.internEmail, 
+                    profileData.internSchool
+                );
+                this.interns.push(intern);
+            };
+            profiles.newIntern();
+            promptProfiles(); // Call function again
+        }
+        // Else (Exit)
+        else {
+            console.log('Thank you. Your webpage is being generated.');
+            return;
+        }
     });
 }
 
